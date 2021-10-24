8aedd8ed1846caeed4955f46ba795c7335c4d461
kanavdawra <kanavdawra97@gmail.com>
2021-10-24 12:30:24 -0700

    v1.1 (signin-signup)

--- clinicApp/static/js/signup.js
+++ clinicApp/static/js/signup.js
@@ -1,5 +1,5 @@
 var userName, userEmail, userPassword = "", userConfirmPassword = "",
-    userPhone, userAdLine1, userAdLine2, userDoB, userGender = "", userYear,userCreditScore;
+    userPhone, userAdLine1, userAdLine2, userDoB, userGender = "", userYear,userCreditScore,userPerson = "";
 var calender = new Date();
 var year = calender.getFullYear();
 var month = calender.getMonth();
@@ -19,7 +19,7 @@ function Signup() {
 function SetData() {
     console.log("SetData");
     var userData = {
-        "userName": userName.val(), "userGender": userGender,
+        "userName": userName.val(), "userGender": userGender, "userType":userPerson,
         "userDoB": userDoB.val(), "userEmail": userEmail.val(),
         "userPassword": userPassword.val(), "userPhone": userPhone.val(),
         "userAdLine1": userAdLine1.val(), "userAdLine2": userAdLine2.val(),
@@ -29,7 +29,7 @@ function SetData() {
         "userLoanTenure":0,
         "userInterestRate":0,
     };
-    localStorage.setItem(userEmail.val(), JSON.stringify(userData));
+    JSON_Data(userData)
     SendHtml();
 }
 
@@ -61,8 +61,19 @@ function selectGender(gender) {
     document.getElementById("genderButton").innerHTML = gender;
     userGender = gender;
     document.getElementById("genderButton").style.color = "#ffffff";
-    document.getElementById("genderButton").style.backgroundColor = "#28a2a2";
+    document.getElementById("genderButton").style.backgroundColor = "#C01F31";
     document.getElementById("genderButton").style.border = "none";
+    document.getElementById("validationLabel").innerHTML = "";
+
+
+}
+
+function selectPerson(person) {
+    document.getElementById("personButton").innerHTML = person;
+    userPerson = person;
+    document.getElementById("personButton").style.color = "#ffffff";
+    document.getElementById("personButton").style.backgroundColor = "#C01F31";
+    document.getElementById("personButton").style.border = "none";
     document.getElementById("validationLabel").innerHTML = "";
 
 
@@ -70,178 +81,202 @@ function selectGender(gender) {
 
 function Validation() {
 
+
     if(userName.val()===""){
-        document.getElementById("label-name").style.color = "#ff253a";
+
     }
 
     if ( userName.val()!=="" && userGender === "") {
         //Remove validation red
-        document.getElementById("label-name").style.color = "#ccc";
+
         //Add validation red
-        document.getElementById("genderButton").style.color = "#ff253a";
+        document.getElementById("genderButton").style.color = "#ff9100";
         document.getElementById("genderButton").style.backgroundColor = "#ffffff";
-        document.getElementById("genderButton").style.border = "1px solid #ff253a";
+        document.getElementById("genderButton").style.border = "1px solid #ff9100";
         document.getElementById("validationLabel").innerHTML = "Please select your gender";
     }
 
-    if (userName.val()!=="" && userGender !== "" && userDoB.val() === "") {
+    if (userName.val()!=="" && userGender !== "" && userPerson==="") {
 
         //Remove validation red
 
         document.getElementById("genderButton").style.color = "#ffffff";
-        document.getElementById("genderButton").style.backgroundColor = "#28a2a2";
+        document.getElementById("genderButton").style.backgroundColor = "#C01F31";
         document.getElementById("genderButton").style.border = "none";
         document.getElementById("validationLabel").innerHTML = "";
 
         //Add validation red
+        document.getElementById("personButton").style.color = "#ff9100";
+        document.getElementById("personButton").style.backgroundColor = "#ffffff";
+        document.getElementById("personButton").style.border = "1px solid #ff9100";
+        document.getElementById("validationLabel").innerHTML = "Please select your client type";
+    }
 
-        document.getElementById("input-DoB").style.color = "#ff253a";
+    if (userName.val()!=="" && userGender !== "" && userPerson !== "" && userDoB.val() === "") {
+
+        //Remove validation red
+
+        document.getElementById("personButton").style.color = "#ffffff";
+        document.getElementById("personButton").style.backgroundColor = "#C01F31";
+        document.getElementById("personButton").style.border = "none";
+        document.getElementById("validationLabel").innerHTML = "";
+
+        //Add validation red
+
+        document.getElementById("input-DoB").style.color = "#ff9100";
         document.getElementById("input-DoB").style.backgroundColor = "#ffffff";
-        document.getElementById("input-DoB").style.border = "1px solid #ff253a";
+        document.getElementById("input-DoB").style.border = "1px solid #ff9100";
         document.getElementById("validationLabel").innerHTML = "Please select your date of birth";
     }
 
-    if (userName.val()!=="" && userGender !== "" && userDoB.val() !== "" && userYear >= year - 18) {
+    if (userName.val()!=="" && userGender !== "" && userPerson !== "" && userDoB.val() !== "" && userYear >= year - 18) {
 
         //Remove validation red
 
         document.getElementById("input-DoB").style.color = "#ffffff";
-        document.getElementById("input-DoB").style.backgroundColor = "#28a2a2";
+        document.getElementById("input-DoB").style.backgroundColor = "#C01F31";
         document.getElementById("input-DoB").style.border = "none";
         document.getElementById("validationLabel").innerHTML = "";
 
         //Add validation red
 
-        document.getElementById("input-DoB").style.color = "#ff253a";
+        document.getElementById("input-DoB").style.color = "#ff9100";
         document.getElementById("input-DoB").style.backgroundColor = "#ffffff";
-        document.getElementById("input-DoB").style.border = "1px solid #ff253a";
+        document.getElementById("input-DoB").style.border = "1px solid #ff9100";
         document.getElementById("validationLabel").innerHTML = "You must be at least 18 year old";
     }
 
-    if( userName.val()!=="" && userGender !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
+    if( userName.val()!=="" && userGender !== "" && userPerson !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
         userEmail.val()===""){
 
         //Remove validation red
 
         document.getElementById("input-DoB").style.color = "#ffffff";
-        document.getElementById("input-DoB").style.backgroundColor = "#28a2a2";
+        document.getElementById("input-DoB").style.backgroundColor = "#C01F31";
         document.getElementById("input-DoB").style.border = "none";
         document.getElementById("validationLabel").innerHTML = "";
 
         //Add validation red
 
-        document.getElementById("label-email").style.color = "#ff253a";
+
     }
 
     var email=localStorage.getItem(userEmail.val());
 
-    if( userName.val()!=="" && userGender !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
+    if( userName.val()!=="" && userGender !== "" && userPerson !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
         userEmail.val()!=="" && email!==null){
 
         //Remove validation red
 
-        document.getElementById("label-email").style.color = "#ccc";
+
 
         //Add validation red
 
-        document.getElementById("label-email").style.color = "#ff253a";
+
         document.getElementById("validationLabel").innerHTML = "Email already exists";
     }
 
-    if( userName.val()!=="" && userGender !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
+    if( userName.val()!=="" && userGender !== "" && userPerson !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
         userEmail.val()!=="" && email===null && userPassword.val()===""){
 
         //Remove validation red
 
-        document.getElementById("label-email").style.color = "#ccc";
+
         document.getElementById("validationLabel").innerHTML = "";
 
         //Add validation red
 
-        document.getElementById("label-password").style.color = "#ff253a";
+
     }
 
-    if( userName.val()!=="" && userGender !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
+    if( userName.val()!=="" && userGender !== "" && userPerson !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
         userEmail.val()!=="" && email===null && userPassword.val()!=="" && userConfirmPassword.val()===""){
 
         //Remove validation red
 
-        document.getElementById("label-password").style.color = "#ccc";
+
 
         //Add validation red
 
-        document.getElementById("label-confirmPassword").style.color = "#ff253a";
+
     }
 
-    if( userName.val()!=="" && userGender !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
+    if( userName.val()!=="" && userGender !== "" && userPerson !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
         userEmail.val()!=="" && email===null && userPassword.val()!=="" && userConfirmPassword.val()!=="" &&
         userPassword.val()!==userConfirmPassword.val()){
 
         //Remove validation red
 
-        document.getElementById("label-confirmPassword").style.color = "#ccc";
+
 
         //Add validation red
 
-        document.getElementById("label-confirmPassword").style.color = "#ff253a";
-        document.getElementById("label-password").style.color = "#ff253a";
+
         document.getElementById("validationLabel").innerHTML = "Password does no match";
     }
 
-    if( userName.val()!=="" && userGender !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
+    if( userName.val()!=="" && userGender !== "" && userPerson !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
         userEmail.val()!=="" && email===null && userPassword.val()!=="" && userConfirmPassword.val()!=="" &&
         userPassword.val()===userConfirmPassword.val() && userPhone.val()===""){
 
         //Remove validation red
 
-        document.getElementById("label-confirmPassword").style.color = "#ccc";
-        document.getElementById("label-password").style.color = "#ccc";
+
         document.getElementById("validationLabel").innerHTML = "";
 
         //Add validation red
 
-        document.getElementById("label-phNo").style.color = "#ff253a";
+
     }
 
-    if( userName.val()!=="" && userGender !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
+    if( userName.val()!=="" && userGender !== "" && userPerson !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
         userEmail.val()!=="" && email===null && userPassword.val()!=="" && userConfirmPassword.val()!=="" &&
         userPassword.val()===userConfirmPassword.val() && userPhone.val()!=="" && userPhone.val().length!==10){
 
         //Remove validation red
 
-        document.getElementById("label-phNo").style.color = "#ccc";
+
 
         //Add validation red
 
-        document.getElementById("label-phNo").style.color = "#ff253a";
+
         document.getElementById("validationLabel").innerHTML = "Invalid Phone number entered";
     }
 
-    if( userName.val()!=="" && userGender !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
+    if( userName.val()!=="" && userGender !== "" && userPerson !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
         userEmail.val()!=="" && email===null && userPassword.val()!=="" && userConfirmPassword.val()!=="" &&
         userPassword.val()===userConfirmPassword.val() && userPhone.val()!=="" && userPhone.val().length===10 &&
         userAdLine1.val()===""){
 
         //Remove validation red
 
-        document.getElementById("label-phNo").style.color = "#ccc";
+
         document.getElementById("validationLabel").innerHTML = "";
 
         //Add validation red
 
-        document.getElementById("label-adLine1").style.color = "#ff253a";
+
     }
 
-    if( userName.val()!=="" && userGender !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
+    if( userName.val()!=="" && userGender !== "" && userPerson !== "" && userDoB.val() !== "" && userYear <= year - 18 &&
         userEmail.val()!=="" && email===null && userPassword.val()!=="" && userConfirmPassword.val()!=="" &&
         userPassword.val()===userConfirmPassword.val() && userPhone.val()!=="" && userPhone.val().length===10 &&
         userAdLine1.val()!==""){
 
         //Remove validation red
 
-        document.getElementById("label-adLine1").style.color = "#ccc";
+
 
         SetData();
     }
 
 }
+
+function JSON_Data(data) {
+
+    // if email exists
+
+    // then store data in firebase
+
+}
+
