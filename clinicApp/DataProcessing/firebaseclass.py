7756c63613bf08db10381f94e06460774a7e68ce
rohan <singla553@gmail.com>
2021-11-05 17:49:31 -0700

    Version containing Login and Sign up v3

---
+++ clinicApp/DataProcessing/firebaseclass.py
@@ -0,0 +1,53 @@
+import pyrebase
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin.auth import UserNotFoundError, ExpiredIdTokenError
+
+
+class firebaseconfig:
+
+    def __init__(self):
+        firebaseConfig = {'apiKey': "AIzaSyBRAmbOE0Je6pYBet1poJrssEgVHIy79Wk",
+                          'authDomain': "django-51dc3.firebaseapp.com",
+                          'databaseURL': "https://django-51dc3-default-rtdb.firebaseio.com",
+                          'projectId': "django-51dc3",
+                          'storageBucket': "django-51dc3.appspot.com",
+                          'messagingSenderId': "606023351895",
+                          'appId': "1:606023351895:web:a984b45afde710d1996d12",
+                          'measurementId': "G-17ZG54CB2H"}
+
+        self.firebase = pyrebase.initialize_app(firebaseConfig)
+        self.database = self.firebase.database()
+        self.auth = self.firebase.auth()
+        self.cred = credentials.Certificate({
+
+}
+)
+        firebase_admin.initialize_app(self.cred)
+
+    def getjson(self, myjson3):
+        email = myjson3.get("userEmail")
+        key = self.emailfetching(email)
+        password = myjson3.get("userPassword")
+        user = self.auth.create_user_with_email_and_password(email, password)
+        self.database.child(key).set(myjson3)
+
+    def emailfetching(self, email):
+        updatedemail = email.split("@")[0]
+
+        return updatedemail
+
+    def signin(self, myjson3):
+        email = myjson3.get("userEmail")
+        password = myjson3.get("userPassword")
+        try:
+            self.auth.sign_in_with_email_and_password(email, password)
+            return "Loggeg in"
+        except:
+            return "invalid User details"
+
+
+
+
